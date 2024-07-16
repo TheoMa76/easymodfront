24a407f9be55e07752e312067994502d7f8b0d6b
Theo Maerten <theo.maerten@sga-industires.com>
2024-07-16 15:16:42 +0200

    login work need middleware + set cookie with token

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
-
-type Props = {}
-
-export default function Page({}: Props) {
+'use client'
+import { FormEvent } from 'react'
+import { useRouter } from 'next/navigation'
+import MinecraftInput from '@/components/atoms/Inputs/MinecraftInput'
+import MinecraftButton from '@/components/atoms/Buttons/MinecraftButton'
+import { log } from 'console'
+ 
+export default function LoginPage() {
+  const router = useRouter()
+ 
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+ 
+    const formData = new FormData(event.currentTarget)
+    const username = formData.get('username')
+    const password = formData.get('password')
+ 
+    const response = await fetch('https://localhost:8000/api/login_check', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password }),
+    })
+ 
+    if (response.ok) {
+      console.log(response);
+      
+      router.push('/')
+    } else {
+      console.log(JSON.stringify({username, password}));
+      
+      console.log(response);
+      
+    }
+  }
+ 
   return (
-    <p>login page</p>
+    <form onSubmit={handleSubmit}>
+      <MinecraftInput type="username" name="username" placeholder="E-mail" isRequired = {true} />
+      <MinecraftInput type="password" name="password" placeholder="Mot de passe" isRequired = {true} />
+      <MinecraftButton label="Se connecter" type="submit"></MinecraftButton>
+    </form>
   )
 }
--- src/components/atoms/Inputs/MinecraftInput.tsx
+++ src/components/atoms/Inputs/MinecraftInput.tsx
@@ -1,5 +1,5 @@
-'use client'
-import React, { useState } from 'react';
+'use client';
+import React, { ChangeEvent } from 'react';
 
 type MinecraftInputVariant = 'primary' | 'secondary' | 'neutral';
 
@@ -7,13 +7,15 @@ interface MinecraftInputProps {
   variant?: MinecraftInputVariant;
   placeholder?: string;
   value?: string;
-  onChange?: () => void;
-  onInput?: () => void;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  onInput?: (event: ChangeEvent<HTMLInputElement>) => void;
   disabled?: boolean;
   className?: string;
+  name?: string;
   icon?: any;
   label?: string;
   type?: string;
+  isRequired?: boolean;
 }
 
 const MinecraftInput: React.FC<MinecraftInputProps> = ({
@@ -27,6 +29,8 @@ const MinecraftInput: React.FC<MinecraftInputProps> = ({
   icon,
   label = "Exemple input",
   type = 'text',
+  name = label,
+  isRequired = false,
 }) => {
   const baseClasses =
     'appearance-none border-2 rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline';
@@ -38,20 +42,20 @@ const MinecraftInput: React.FC<MinecraftInputProps> = ({
           {icon}
         </div>
       )}
-      <label htmlFor={label} className="text-custom-dark"></label>
-      <MinecraftInput
+      <label htmlFor={label} className="text-custom-dark">{label}</label>
+      <input
+        id={label}
         type={type}
         className={`${baseClasses} ${className} ${icon ? 'pl-10' : ''}`}
         placeholder={placeholder}
         aria-label={`${placeholder} + ${label}`}
         value={value}
+        onChange={onChange}
+        onInput={onInput}
         disabled={disabled}
+        name = {name}
+        required = {isRequired}
       />
-      {value && type !== 'password' &&   (
-        <p className="text-2xl mt-2 text-gray-700">
-          Vous avez saisi : <strong>{value}</strong>
-        </p>
-      )}
     </div>
   );
 };
